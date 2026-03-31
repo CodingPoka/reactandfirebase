25703504f2653616118a173bf5e16247cb8f84d5
CodingPoka <kingshukbd.01@gmail.com>
2026-03-31 20:25:55 +0600

    showing data to dashboard page

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,21 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
-
-  const [user,setUser]= useState(null);
-
+  const [user, setUser] = useState(null);
 
   //using useEffect to get the user data from localStorage when the component mounts
-  useEffect(()=>{
-    
+  useEffect(() => {
     //get the use data from localStorage and store it in userData variable
-     const userData = localStorage.getItem("user");
+    const userData = localStorage.getItem("user");
 
-  })
+    if (userData) {
+      //convert the localstorage data from string to object using JSON.parse and store it in user state variable using setUser function
+      setUser(JSON.parse(userData));
+    }
+  }, []);
 
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+      <h1 className="text-center text-3xl py-10 text-red-400">
+        Welcome To Dashboard
+      </h1>
 
-export default Dashboard
+      <div>
+        {user ? (
+          <div>
+            <h1 className="text-center text-2xl font-bold">
+              Name : {user.name}
+            </h1>
+            <h1 className="text-center text-2xl font-bold">
+              Email : {user.email}
+            </h1>
+          </div>
+        ) : (
+          <div>
+            {" "}
+            <h1 className="text-center text-2xl font-bold">User not found</h1>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
